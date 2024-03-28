eb621c99d5c595f2625a1cc9ffb4fd933b6e6917
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-03-28 16:12:28 -0300

    Create Lista-2_6-POO.cpp

---
+++ Lista-2_6-POO.cpp
@@ -0,0 +1,12 @@
+6 - Por que usar o encapsulamento no seu programa? Não esqueça de mencionar
+em sua resposta os Modificadores de Visibilidade (+publico, -privado,
+#protegido).
+
+O encapsulamento, junto aos modificadores de visibilidade (+public, -private, #protected), 
+é uma prática fundamental na programação orientada a objetos que promove a segurança, 
+a abstração, a manutenção, a reutilização e o controle de acesso em seus programas.
+
+
+
+
+
