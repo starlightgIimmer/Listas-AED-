d3772a97510ab15eae2dacab67b9c5f5af275816
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-03-28 16:15:27 -0300

    Create Lista-2_7-POO.cpp

---
+++ Lista-2_7-POO.cpp
@@ -0,0 +1,21 @@
+7 - Para que são utilizados os métodos getters e setters? Exemplifique utilizando
+a notação gráfica de uma classe, criando métodos getters e setters para cada dos
+atributos da sua classe exemplo.
+  Os métodos getters e setters são utilizados para acessar e modificar os atributos 
+de uma classe de forma controlada, seguindo os princípios de encapsulamento. Os getters 
+são métodos que permitem obter o valor de um atributo, enquanto os setters são métodos
+que permitem alterar o valor de um atributo.
+   ___________________________________________
+|                 Pessoa                    |
+|-------------------------------------------|
+| - nome : string                           |
+| - idade : int                             |
+| - sexo : char                             |
+|-------------------------------------------|
+| + getNome() : string                      |
+| + setNome(nome: string) : void            |
+| + getIdade() : int                        |
+| + setIdade(idade: int) : void             |
+| + getSexo() : char                        |
+| + setSexo(sexo: char) : void              |
+ -------------------------------------------
