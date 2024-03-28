227e51ff0798d15589f430c8c678034d408889cc
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-03-28 16:09:40 -0300

    Create Lista2_3-POO.cpp

---
+++ Lista2_3-POO.cpp
@@ -0,0 +1,22 @@
+3 - Explique o que são atributos e métodos de uma classe e, exemplifique
+utilizando a notação gráfica de uma classe (POO - Objetos – Classes [Slide 31]).
+
+Os atributos são as características que definem um objeto. Eles representam o estado do
+objeto e são representados por variáveis dentro da classe. Por exemplo, em uma classe 
+"Carro", os atributos podem incluir "marca", "modelo", "cor", "ano", etc.
+Já os métodos são as ações que um objeto pode realizar. Eles representam o comportamento do 
+objeto e são representados por funções dentro da classe. Por exemplo, em uma classe "Carro",
+os métodos podem incluir "ligar", "desligar", "acelerar", "frear", etc.
+ ___________________________________________
+|                Carro                      |
+|-------------------------------------------|
+| - marca : string                          |
+| - modelo : string                         |
+| - cor : string                            |
+| - ano : int                               |
+|-------------------------------------------|
+| + ligar() : void                          |
+| + desligar() : void                       |
+| + acelerar(velocidade: int) : void        |
+| + frear() : void                          |
+ -------------------------------------------
