88eb943c2c12b9dbcecc025c9378ad3cdca7b598
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-03-28 13:35:25 -0300

    Create Lista2_4-POO.cpp

---
+++ Lista2_4-POO.cpp
@@ -0,0 +1,12 @@
+4 - Identifique a opção que melhor define as três características elementares de
+um objeto:
+a) Hierarquia, polimorfismo de sobrecarga, polimorfismo de sobreposição.
+b) Método estático, variável e atributos.
+c) Estado, comportamento e identidade.
+d) Público, privado e friend.
+e) Não quero mais fazer códigos.
+f) Nenhuma das alternativas.
+A resposta correta é a alternativa c) Estado, comportamento e identidade, que definem o que são os objetos.
+O estado se refere aos atributos e seus respectivos valores, o comportamento é como o objeto reage a 
+mudança de estado e relacionamento com as demais partes do sistema e a identidade reflete as propriedades desse objeto
+que fazem ele se distinguir dos demais.
