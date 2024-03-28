b257690373a9a658f5baa6c0de37296fe05b0940
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-03-28 13:38:00 -0300

    Create Lista-2_5-POO.cpp

---
+++ Lista-2_5-POO.cpp
@@ -0,0 +1,11 @@
+5 - Defina a diferença entre herança e polimorfismo na POO.
+Na programação orientada a objetos, a herança permite que uma classe herde características e comportamentos de outra classe,
+enquanto o polimorfismo permite que objetos de classes diferentes possam ser tratados de maneira uniforme, possibilitando que 
+métodos de uma classe base sejam substituídos por métodos de suas subclasses.
+
+
+
+
+
+
+
