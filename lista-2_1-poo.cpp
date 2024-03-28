984e5631c6f11a1b38f49b8e552bcb77d69217b3
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-03-28 13:26:36 -0300

    Create lista-2_1-poo.cpp

---
+++ lista-2_1-poo.cpp
@@ -0,0 +1,16 @@
+1 - Conceitue e exemplifique os seguintes itens:
+a) Programação Orientada a Objetos (POO)
+A programação orientada a objetos (POO) é um paradigma de programação que organiza o código em torno de objetos, 
+que representam entidades do mundo real. A POO promove a reutilização de código e a modularidade,
+encapsulando dados e funcionalidades dentro dos objetos. Um exemplo simples de programação orientada a objetos em C++, 
+envolve a criação de uma classe Pessoa com atributos como nome e idade, e métodos para definir e obter esses atributos que podem ser úteis em cadastros.
+b) Classe
+Uma classe é uma categorização de objetos que define atributos e métodos para os objetos dessa classe. Usando o item anterior, 
+a classe Pessoa define atributos como nome, idade, CPF, etc. Ela também pode definir métodos para obter e imprimir esses atributos
+em algum lugar.
+c) Instância
+Uma instância refere-se a um objeto específico criado a partir de uma classe. Um exemplo de instância pode ser um objeto P1 da classe Pessoa que possuísse
+os atributos nome e idade como João e 22.
+d) Objeto
+Cada objeto é uma instância de uma classe, que define suas 
+características (atributos) e comportamentos (métodos).
