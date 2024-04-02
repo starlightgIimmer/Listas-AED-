62ac8e488b193ec3b14929b3f7f59a38899932a1
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-02 16:25:12 -0300

    Create Lista-3_4-Complexidade.cpp

---
+++ Lista-3_4-Complexidade.cpp
@@ -0,0 +1,5 @@
+4. Suponha um algoritmo A e um algoritmo B com funções de complexidade de tempo
+a(n) = n^2 - n + 549 e b(n) = 49n + 49, respectivamente. Determine quais são os valores 
+de n pertencentes ao conjunto dos números naturais para os quais A leva menos tempo 
+para executar do que B.
+  Para encontrar 
