dd04e0ab83a3e4af2f3774c57ba7a35916ffbb89
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-02 15:55:34 -0300

    Create Lista-3_1-.cpp

---
+++ Lista-3_1-.cpp
@@ -0,0 +1,3 @@
+1. O que significa dizer que uma função g(n) é O(f(n))?
+  Isso significa que f(n) domina assintóticamente g(n), ou seja,
+é falar que g(n) não cresce tão rápido quanto f(n) e que g(n) é de ordem de no máximo f(n).
