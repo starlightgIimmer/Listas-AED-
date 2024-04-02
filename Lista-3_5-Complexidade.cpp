02abdf6cc34db2468b14b8e0feeec9c59978e71f
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-02 16:40:42 -0300

    Create Lista-3_5-Complexidade.cpp

---
+++ Lista-3_5-Complexidade.cpp
@@ -0,0 +1,9 @@
+Para expressar a função \(10n^3 - 5n^2 - 10n + 3\) em termos da notação \( \Theta \), precisamos encontrar funções superior e inferior que sejam assintoticamente limitadas pela função dada.
+
+Vamos analisar os termos dominantes da função \(10n^3 - 5n^2 - 10n + 3\). O termo dominante, que determina o comportamento assintótico da função, é \(10n^3\). Portanto, podemos expressar a função em termos de \(n^3\):
+
+\[ 10n^3 - 5n^2 - 10n + 3 = \Theta(n^3) \]
+
+Isso ocorre porque, para valores suficientemente grandes de \(n\), o termo \(10n^3\) domina todos os outros termos da expressão.
+
+Portanto, a função \(10n^3 - 5n^2 - 10n + 3\) pode ser expressa em termos da notação \( \Theta \) como \( \Theta(n^3) \).
