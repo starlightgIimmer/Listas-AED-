63fc096e6e6b0b4b689bb83ff544a2a7d3d73dc1
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-02 16:29:16 -0300

    Create Lista-3_9-Complexidade.cpp

---
+++ Lista-3_9-Complexidade.cpp
@@ -0,0 +1,4 @@
+9. Quais as grandezas físicas que influenciam a eficiência de tempo de um algoritmo na prática?
+  Algumas grandezas físicas que podem influenciar a eficiência de tempo de um algoritmo são:
+a memória cache, o tamanho e a velocidade da memória RAM, a velocidade e o número de núcleos
+do processador, o tamanho e a natureza dos dados, a otimização do compilador, etc.
