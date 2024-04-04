41657320b02e1dda3b7c33f22b61d37179868fc5
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-04 10:50:39 -0300

    Create Lista-3_2.cpp

---
+++ Lista-3_2.cpp
@@ -0,0 +1,4 @@
+2. O que significa dizer que uma função g(n) é Θ(f(n))?
+  Dizer que uma função g(n) é Θ(f(n)) significa que ela está limitada
+tanto superiormente quanto inferiormente por f(n). Elas crescem na mesma
+taxa quando n tende ao infinito. É o caso médio.
