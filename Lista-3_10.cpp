cb62176cc6b5a1264ce2a4113f5bc6881e7002d2
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-04 11:04:00 -0300

    Create Lista-3_10.cpp

---
+++ Lista-3_10.cpp
@@ -0,0 +1,9 @@
+10. Para o cálculo da complexidade de algoritmos não recursivos, existe um conjunto de
+regras bastante simples de serem seguidas. Cite e descreva estas regras.
+  A primeira regra consiste em contar as operações básicas dentro do algorito, por exemplo
+alguma soma, comparação, subtração, etc.
+  Em seguida é necessário identificar o ciclo dominante, analisando os loops do algoritmo.
+  A partir disso, realiza-se uma contagem do número de loops desse ciclo dominante em função
+do tamanho da entrada n.
+  Após isso, expressar a complexidade em função de n do algoritmo.
+  Por último, de acordo com a expressão de complexidade, classificar essa complexidade.
