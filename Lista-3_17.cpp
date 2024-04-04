1183fa15b7690fe4ed2f6044825ed7873fa98aeb
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-04 11:40:28 -0300

    Create Lista-3_17.cpp

---
+++ Lista-3_17.cpp
@@ -0,0 +1,8 @@
+17. Obtenha a equação matemática referente a análise do pior e melhor caso do fragmento
+de código abaixo:
+1 for ( i = 0 ; i < N; i ++)
+2   printf ( ”%d” , i ) ;
+
+  O pior caso do problema é i ser igual a N. Então sua ordem é O(N).
+Já o melhor caso é a condição não ser satisfeita ou seja i é maior que zero.
+Sua ordem no melhor caso é O(1).
