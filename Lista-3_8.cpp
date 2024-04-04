751ecd888febf91a57be254948fb07eea0491c24
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-04 10:40:07 -0300

    Create Lista-3_8.cpp

---
+++ Lista-3_8.cpp
@@ -0,0 +1,22 @@
+8. Qual a ordem de complexidade no pior caso de:
+
+(a) 2n + 10
+2n + 10 = O(n) pois é uma função linear.
+
+(b) (1/2)n(n + 1)
+Essa expressão possui n(n+1) e isso é igual a n^2+n.
+Por causa disso ela é tem complexidade polinomial e sua ordem de complexidade no pior caso é O(n^2).
+
+(c) n + √n
+Como o termo dominante é n, a ordem de complexidade no pior caso é igual a O(n).
+
+(d) n/1000
+A função n/1000 é uma função linear, então sua ordem de complexidade é igual a O(n).
+
+(e) (1/2)n^2
+Ela possui complexidade polinomial devido ao n elevado ao quadrado, ou seja, sua ordem
+de complexidade é O(n^2).
+
+(f) (1/2)n^2 − 3n
+Esse algoritmo também possui complexidade polinomial devido ao n elevado ao quadrado, então, sua ordem
+de complexidade é O(n^2).
