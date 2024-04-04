61db236286f96f3501cf899254ce37ee2571f627
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-04 11:13:52 -0300

    Create Lista-3_11.cpp

---
+++ Lista-3_11.cpp
@@ -0,0 +1,4 @@
+11. Explique que tipos de problemas ou algoritmos costumam ter complexidade da ordem de n.log(n) e como os identificamos.
+Essa ordem de complexidade é quase linear, mas multiplicado por um fator logarítmico.
+Isso é altamente eficiente e geralmente mais rápido do que algoritmos com complexidade quadrática para grandes conjuntos
+de dados. Então ela é comum em problemas de divisão e conquista, em algoritmos de ordenação, etc.
