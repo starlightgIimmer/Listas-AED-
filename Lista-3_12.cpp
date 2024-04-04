bddfb18f1c1275f7502e2909ce8b84ed0a7e5271
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-04 11:16:31 -0300

    Create Lista-3_12.cpp

---
+++ Lista-3_12.cpp
@@ -0,0 +1,4 @@
+12. Quais problemas que possuem geralmente complexidade da ordem de logn?
+  Problemas e algoritmos que envolvem busca binária, operações em árvores 
+binárias de busca balanceadas e divisão e conquista com divisões de 
+tamanho constante.
