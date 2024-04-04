0e37603ea5a0da2b8c4ebf08890f358e68d9ab7c
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-04 10:27:06 -0300

    Create Lista-3_7.cpp

---
+++ Lista-3_7.cpp
@@ -0,0 +1,3 @@
+7. Dois algoritmos A e B possuem complexidade n^5 e 2^n respectivamente. Você utilizaria o algoritmo B ao invés do A, em qual caso? Explique.
+  O algoritmo B é mais complexo do que o algoritmo A por se tratar de uma função exponencial, então ele aumenta de tamanho muito mais rápido que A.
+ É melhor optar pelo algoritmo B apenas se o tamanho do problema for pequeno o suficiente para que sua complexidade exponencial não se torne um problema.
