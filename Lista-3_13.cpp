faf9dce0821d47f2f80368b9c93281ad91afcd1d
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-04 11:19:33 -0300

    Create Lista-3_13.cpp

---
+++ Lista-3_13.cpp
@@ -0,0 +1,4 @@
+13. Quais problemas que costumam ser exponenciais?
+  Os problemas que geralmente têm complexidade exponencial são caracterizados 
+pelo crescimento rápido do tempo de execução à medida que o tamanho da entrada aumenta.
+  São problemas que muitas vezes são difíceis de resolver devido ao tamanho das saídas.
