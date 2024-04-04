2f73ccf94c60bc8b83171bd38b727180e9a2622f
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-04 10:52:26 -0300

    Delete Lista-3_3-.cpp

--- Lista-3_3-.cpp
+++
@@ -1,4 +0,0 @@
-3. O que significa dizer que uma função g(n) é Ω(f(n))?
-  Dizer que g(n) é Ω(f(n)) significa que f(n) é o limite inferior
-de g(n), ou seja, f(n) é dominada assintóticamente por g(n). É 
-conhecido como pior caso.
