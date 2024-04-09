397a291f7a48059a7a7b626bc8316d05cadd099c
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-09 11:37:30 -0300

    Create Lista-4_10.cpp

---
+++ Lista-4_10.cpp
@@ -0,0 +1,3 @@
+Questão 10: Escreva uma função que procure o valor N no vetor criado acima.
+Retorne a posição do valor N no vetor e retorne quantas pesquisas foram feitas?
+Você utilizou busca sequencial, busca binária ou busca com sentinela? Por quê?
