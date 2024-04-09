715082482f62263c2b3fc5da1421bd9753b2f809
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-09 14:33:04 -0300

    Update Lista-4_6.cpp

--- Lista-4_6.cpp
+++ Lista-4_6.cpp
@@ -2,6 +2,8 @@ Questão 6: Escreva um código que verifique quantas pesquisas são feitas em
 cada tipo de pesquisa. Qual é a que faz menos pesquisas? Qual faz mais
 pesquisas?
 
+*nao esta achando a posicao da maior pesquisa
+
 #include <iostream>
 #include <string>
 
