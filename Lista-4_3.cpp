2ec5f08d587e94bfe65a93e286502a664b5d4c91
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-09 11:15:18 -0300

    Create Lista-4_3.cpp

---
+++ Lista-4_3.cpp
@@ -0,0 +1,25 @@
+Questão 3: Escreva uma função pesqseq(int val) que procura o valor val no vetor
+usando pesquisa sequencial e retorna a posição em que ele foi encontrado, ou -1 se não
+está no vetor.
+
+#include <iostream>
+
+using namespace std;
+
+int PesqSeq(int x, int N, int a[])
+{
+    int i = 0;
+    
+    while (i<N && a[i] != x)
+        i++;
+    return (i == N) ? -1 : i;
+}
+int main(void)
+{
+    const int m = 8;
+    int v[m] = {2, 5, 7, 9, 1, 3, 4, 6};
+    
+    cout<<"Busca de 9 = "<< PesqSeq(9, m, v);
+
+    return 0;
+}
