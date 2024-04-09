d5104c4c3181f28d131d18995a6500cc4c287c1f
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-09 10:32:36 -0300

    Create Lista-4_1.cpp

---
+++ Lista-4_1.cpp
@@ -0,0 +1,15 @@
+Questão 1: Escreva um programa que cria um vetor vet (do tamanho que você
+preferir) e preenche o vetor com inteiros.
+ 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int n = 5;
+    int v[n] = {7, 2, 45, 23, 76};
+ 
+    return 0;
+    
+}    
