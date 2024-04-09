dd38be8af7b213c1568da6063c4481f29b7868e3
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-09 10:47:10 -0300

    Update Lista-4_1.cpp

--- Lista-4_1.cpp
+++ Lista-4_1.cpp
@@ -2,13 +2,14 @@ Questão 1: Escreva um programa que cria um vetor vet (do tamanho que você
 preferir) e preenche o vetor com inteiros.
  
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
     const int n = 5;
-    int v[n] = {7, 2, 45, 23, 76};
+    vector<n> vet {7, 2, 45, 23, 76};
  
     return 0;
     
