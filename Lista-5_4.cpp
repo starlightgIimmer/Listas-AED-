cfb14cfa12712da7e5ecc368aa7449c46411d66f
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-16 10:21:20 -0300

    Update Lista-5_4.cpp

--- Lista-5_4.cpp
+++ Lista-5_4.cpp
@@ -4,3 +4,17 @@ servirão como parâmetro para a encomendada de bebedouros de alturas
 especificas para conseguir atender todas as pessoas eficientemente, da
 menor até a maior. Para a programação, você deve utilizar a ordenação
 BubbleSort.
+  Nesse desafio deve-se mostrar todas as alturas coletadas e como saída,
+deve-se mostrar todas as alturas ordenadas em ordem crescente. Além
+disso, para o cálculo dos números aleatórios deve-se usar as bibliotecas e 2
+comandos:
+
+#include<time.h>
+
+#include<stdlib.h>
+
+srand(time(NULL));
+Essa função gera um número aleatório de acordo com o tempo.
+
+  n+rand()%k;
+Essa função diz o alcance dos números sorteados: de n ate k
