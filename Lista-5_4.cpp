7b2ad093c5d7db7ff779c305c1d39a029724c83f
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-16 10:19:52 -0300

    Create Lista-5_4.cpp

---
+++ Lista-5_4.cpp
@@ -0,0 +1,6 @@
+Questão 4: Este desafio consiste em calcular 100 alturas de alunos geradas
+aleatoriamente entre 1-3 metros com até duas casas decimais. Essas alturas
+servirão como parâmetro para a encomendada de bebedouros de alturas
+especificas para conseguir atender todas as pessoas eficientemente, da
+menor até a maior. Para a programação, você deve utilizar a ordenação
+BubbleSort.
