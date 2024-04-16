f029a34b574b4f97968a982af98f136defb8b429
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-16 10:19:28 -0300

    Create Lista-5_3.cpp

---
+++ Lista-5_3.cpp
@@ -0,0 +1,9 @@
+Questão 3: Considere a seguinte estrutura:
+struct pessoa{
+int Matricula;
+char Nome[30];
+float Nota;
+};
+Faça uma função que dado um array de tamanho N dessa estrutura, ordene
+o array pelo campo escolhido pelo usuário. A ordenação deve utilizar o
+método Merge Sort.
