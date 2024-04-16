618a908bf6d7166666cb5d6b8ffa63288d20941e
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-04-16 10:18:34 -0300

    Create Lista-5_1.cpp

---
+++ Lista-5_1.cpp
@@ -0,0 +1,3 @@
+Questão 1: Faça um programa que leia n nomes inserindo-os em uma lista
+de forma ordenada utilizando a ideia do algoritmo Insertion Sort. No final,
+o programa deve mostrar todos os nomes ordenados alfabeticamente.
