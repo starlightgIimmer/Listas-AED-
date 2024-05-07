d22cb7c72838f51a5eb71c3995330435ee96d657
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-05-07 10:25:01 -0300

    Create Lista-6_1.cpp

---
+++ Lista-6_1.cpp
@@ -0,0 +1,6 @@
+Questão 1: Escreva um programa que recebe duas listas encadeadas de
+inteiros e efetue os seguintes passos:
+  a) Verifique se as listas estão ordenadas;
+  b) Ordene as listas, caso não estejam ordenadas;
+  c) Mescle os elementos da segunda lista na primeira, mantendo a
+ordenação na lista final.
