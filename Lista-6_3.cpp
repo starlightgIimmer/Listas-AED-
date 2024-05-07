a4a9d238dd8a7309cef1af3c4102780cecda86e9
Pedro Freire Paiter <149101168+starlightgIimmer@users.noreply.github.com>
2024-05-07 10:26:22 -0300

    Create Lista-6_3.cpp

---
+++ Lista-6_3.cpp
@@ -0,0 +1,8 @@
+Questão 3: Escreva um programa que simule o controle de uma pista de
+decolagem de aviões em um aeroporto. Neste programa, o usuário deve ser
+capaz de realizar as seguintes tarefas:
+  a) Listar o número de aviões aguardando na fila de decolagem;
+  b) Autorizar a decolagem do primeiro avião da fila;
+  c) Adicionar um avião à fila de espera;
+  d) Listar todos os aviões na fila de espera;
+  e) Listar as características do primeiro avião da fila.
